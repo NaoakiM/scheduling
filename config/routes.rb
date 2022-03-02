d46f75fe8913c3b91f4809f8ea536a271dec17f5
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-03-02 19:54:38 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'schedulings/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'schedulings#index'
+  resources :schedulings
 end
