57e624ccebba8765b31f716a0cab586584d6118b
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-03-02 19:40:27 +0900

    コントローラー作成

---
+++ app/controllers/schedulings_controller.rb
@@ -0,0 +1,44 @@
+class SchedulingsController < ApplicationController
+  def index
+    @schedulings = Scheduling.all
+    @scheduling = Scheduling.new
+  end
+
+  def new
+    @scheduling = Scheduling.new
+  end
+
+  def show
+    @scheduling = Scheduling.find(params[:id])
+  end
+
+  def create
+    Scheduling.create(scheduling_parameter)
+    redirect_to schedulings_path
+  end
+
+  def destroy
+    @scheduling = Scheduling.find(params[:id])
+    @scheduling.destroy
+    redirect_to schedulings_path, notice:"削除しました"
+  end
+
+  def edit
+    @scheduling = Scheduling.find(params[:id])
+  end
+
+  def update
+    @scheduling = Scheduling.find(params[:id])
+    if @scheduling.update(scheduling_parameter)
+      redirect_to schedulings_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def scheduling_parameter
+    params.require(:scheduling).permit(:title, :content, :start_time, :place)
+  end
+end
